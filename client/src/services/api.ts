b05dec14321de5cdfc642b0f7d917439e3884e70
Sankhadip005 <sankhadip.mondal05@gmail.com>
2026-09-03 09:10:58 +0530

    chore: configure Vercel SPA routing and dynamic API URL for Render deployment

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,8 +1,12 @@
 import axios from 'axios';
 import { ICategory, ISubject, ICourse, IOneShot } from '../types';
 
+const apiBaseUrl = import.meta.env.VITE_API_URL
+  ? `${import.meta.env.VITE_API_URL.replace(/\/$/, '')}/api`
+  : '/api';
+
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: apiBaseUrl,
   headers: {
     'Content-Type': 'application/json'
   }
